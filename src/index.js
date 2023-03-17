a406719c4775c6438fe1dbfa9040adda7771fcc6
adryelAulas <adryeld20@hotmail.com>
2023-03-17 12:42:07 -0300

    feature 33 cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 
 const routes = require('./routes')
-
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
 
 app.use(express.json())
 
+app.use(cors())
 
 app.use(routes)
 
