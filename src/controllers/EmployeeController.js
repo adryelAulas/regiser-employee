d7890123afd131cab044d8bed5670f7d6943664d
adryelAulas <adryeld20@hotmail.com>
2023-04-12 17:24:04 -0300

    create app employees update

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -1,107 +1,90 @@
-const EmployeeService = require('../services/EmployeeService')
+const EmployeeService = require('../services/EmployeeService');
+const { Router } = require('express');
 
-const { Router } = require('express')
+const EmployeeController = Router();
 
-const EmployeeController = Router()
-
-EmployeeController.get('', async (req, res) => {
-   const { name, position } = req.body
-   const {value} =req.query
-   try {
-      if(value){
-      res.status(200).json(await EmployeeService.getEmployeeByName(value))
-      }else{
-      
-         res.status(200).json(await EmployeeService.index())
-      }
-   } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'EmployeeService.index() or EmployeeService.getEmployeeByName(value) is not working' })
-   }
-})
+EmployeeController.get('/', async (req, res) => {
+  const { value } = req.query;
+  try {
+    if (value) {
+      const employees = await EmployeeService.getEmployeeByName(value);
+      res.status(200).json(employees);
+    } else {
+      const employees = await EmployeeService.index();
+      res.status(200).json(employees);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'EmployeeService.index() or EmployeeService.getEmployeeByName(value) is not working' });
+  }
+});
 
 EmployeeController.get('/:id', async (req, res) => {
-   const { id } = req.params
+  const { id } = req.params;
 
-   try {
-      const existsEmployee = await EmployeeService.existsId(id)
-      if (existsEmployee) {
-         try {
-            res.status(200).json(await EmployeeService.show(id))
-         } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: 'EmployeeService.show(id) is not working' })
-         }
-      } else {
-         res.status(404).json({ error: `Id: ${id} ot found` })
-      }
+  try {
+    const existsEmployee = await EmployeeService.existsId(id);
+    if (existsEmployee) {
+      const employee = await EmployeeService.show(id);
+      res.status(200).json(employee);
+    } else {
+      res.status(404).json({ error: `Id: ${id} not found` });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' });
+  }
+});
 
-   } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' })
-   }
-})
-
-EmployeeController.post('', (req, res) => {
-   const { name, position } = req.body
-   try {
-      EmployeeService.store({ name, position })
-      res.status(201).json()
-   } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'EmployeeService.store({name, position}) is not working' })
-   }
-})
+EmployeeController.post('/', async (req, res) => {
+  const { name, position } = req.body;
+  try {
+    await EmployeeService.store({ name, position });
+    res.status(201).json({ message: 'Employee created successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'EmployeeService.store({name, position}) is not working' });
+  }
+});
 
 EmployeeController.delete('/:id', async (req, res) => {
-   const { id } = req.params
+  const { id } = req.params;
 
-   try {
-      const existsEmployee = await EmployeeService.existsId(id)
-      if (existsEmployee) {
-         try {
-            const idDeleted = await EmployeeService.destroy(id)
-            if (idDeleted) {
-               res.json({ reponse: `Id ${idDeleted} has been deleted` })
-
-            } else {
-               res.json({ reponse: `Id ${idDeleted} has not been deleted` })
-            }
-         } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' })
-         }
+  try {
+    const existsEmployee = await EmployeeService.existsId(id);
+    if (existsEmployee) {
+      const idDeleted = await EmployeeService.destroy(id);
+      if (idDeleted) {
+        res.json({ response: `Id ${idDeleted} has been deleted.` });
       } else {
-         res.status(404).json({ error: `Id: ${id} ot found` })
+        res.json({ response: `Id ${idDeleted} has not been deleted.` });
       }
-
-   } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' })
-   }
-})
+    } else {
+      res.status(404).json({ error: `Id: ${id} not found` });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' });
+  }
+});
 
 EmployeeController.put('/:id', async (req, res) => {
-   const { id } = req.params
-   const { name, position, created_at } = req.body
-
+   const { id } = req.params;
+   const { name, position, created_at } = req.body;
+ 
    try {
-      const existsEmployee = await EmployeeService.existsId(id)
-      if (existsEmployee) {
-         try {
-            res.status(200).json(await EmployeeService.update({ id, name, position, created_at }))
-         } catch (error) {
-
-         }
-      } else {
-         res.status(404).json({ error: ` EmployeeService.update({name, position, created_at}) is not found` })
-      }
+     const existsEmployee = await EmployeeService.existsId(id);
+     if (existsEmployee) {
+       const updatedEmployee = await EmployeeService.update({ id, name, position, created_at });
+       res.status(200).json(updatedEmployee);
+     } else {
+       res.status(404).json({ error: `Employee with id ${id} not found` });
+     }
    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'EmployeeService.existsId(id) is not working' })
+     console.error(error);
+     res.status(500).json({ error: 'An error occurred while updating the employee' });
    }
-})
-
+ });
 EmployeeController.patch('/:id', async (req, res) => {
    const { id } = req.params
    const { position } = req.body
